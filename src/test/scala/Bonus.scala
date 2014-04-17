c2fa9c803c2abf17bee4bb1a6d2362d348acf941
Vigouroux Thibault <tvigouroux@octo.com>
2014-04-17 10:54:32 +0200

    Added correction of bonus

--- src/test/scala/Bonus.scala
+++ src/test/scala/Bonus.scala
@@ -9,7 +9,8 @@ class Bonus extends HandsOn {
     }
 
     def pascal(c: Int, r: Int): Int = {
-      ???
+       if (c == 0 || c == r) 1
+       else pascal(c - 1, r - 1) + pascal(c, r - 1)
     }
     
     val result = """1
